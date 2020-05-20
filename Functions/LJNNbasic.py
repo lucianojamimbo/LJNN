afead13894c69de98303ebc3155e4ea6e8b5e77c
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-20 18:05:49 +0100

    changed the method for initialising the network

--- Functions/LJNNbasic.py
+++ Functions/LJNNbasic.py
@@ -5,11 +5,21 @@ def sigmoid(z):
 def sigmoidderivative(x):#only ever pass presig into this!
     return (sigmoid(x)*(1-sigmoid(x)))
 def init(sizes):
-    biases = [np.random.randn(y, 1) for y in sizes[1:]]
-    weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
-    delta = np.zeros(len(sizes)-1)#one zero for each non-input layer
-    delta = [[itm] for itm in delta]
+    weights = []
+    for i in range(1, len(sizes)):
+        print(sizes[i-1], sizes[i])
+        weights.append(np.zeros((sizes[i], sizes[i-1])))
+    biases = []
+    for i in range(1, len(sizes)):
+        biases.append(np.zeros((sizes[i], 1)))
+    delta = []
+    for i in range(1, len(sizes)):
+        delta.append(np.zeros((sizes[i], 1)))
     return weights, biases, delta
+
+
+
+
 def feedforwards(w, b, inp):
     a = inp
     activations = []
@@ -22,15 +32,15 @@ def feedforwards(w, b, inp):
         activations.append(a)
         presig.append(bs)
     return activations, presig
+
+
+
+
 def geterror(activations, desiredoutput, presig, delta, weights):#calculate delta
     delta[-1] = np.multiply((np.subtract(activations[-1], desiredoutput)), sigmoidderivative(presig[-1]))
     i = 1 #start at one because error in output is already done
     while i < len(delta):
+        print(delta[i-1])
         delta[-i-1] = np.multiply(np.transpose(np.matmul(weights[-i].T, delta[-i])), sigmoidderivative(presig[-i-1]))
-        
-        
-        
-        
-        
         i+=1
     return delta
