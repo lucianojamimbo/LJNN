46e9da10b06b9f530734e038f2d34eff054e270c
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-21 19:13:25 +0100

    changed stuff for testing

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import numpy as np
 import Functions.LJNNbasic as ljnnb
 import Algorithms.Train as train
 np.random.seed(0)
-sizes = [1,2,2]
+sizes = [1,2,3]
+
 weights, biases, delta = ljnnb.init(sizes)
 
-activations, presig = ljnnb.feedforwards(weights, biases, [1])
-delta = ljnnb.geterror(activations, [0,0], presig, delta, weights)
-eta = 1
+a, ps = ljnnb.feedforwards(weights, biases, [[1]])
 
-print("before", biases)
-train.SimpleGradientDescent(activations, delta, weights, biases, 1)
-print("after", biases)
+do = [[1],[1],[1]]
+
+
+
+
+delta = ljnnb.geterror(a, do, ps, delta, weights)
