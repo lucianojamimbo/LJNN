80a9337bf78724cff69f91916a3d2707437131db
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-21 20:29:41 +0100

    SimpleGD is now a thing that probably works

--- Algorithms/Train.py
+++ Algorithms/Train.py
@@ -1,4 +1,6 @@
 import numpy as np
 import copy
 def SimpleGradientDescent(activations, delta, weights, biases, eta):
+    weights = np.subtract(weights, np.multiply(eta, np.multiply(activations[1:], delta)))
+    biases = np.subtract(biases, np.multiply(eta, delta))
     return weights, biases
