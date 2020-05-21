b9a83e63847cd165b88502579c6e51d8abf13141
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-21 21:56:19 +0100

    added more testing of stuff

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ a, ps = ljnnb.feedforwards(weights, biases, [[1]])
 
 do = [[1],[1],[1]]
 
-
-
-
 delta = ljnnb.geterror(a, do, ps, delta, weights)
+i = 0
+while i < 1000:
+    a, ps = ljnnb.feedforwards(weights, biases, [[1]])
+    delta = ljnnb.geterror(a, do, ps, delta, weights)    
+    weights, biases = train.SimpleGradientDescent(a, delta, weights, biases, 2)
+    i+=1
