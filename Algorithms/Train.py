b895756833972c3908cd999a393aa4a975b31615
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-23 19:29:46 +0100

    simple GD is 100% working!

--- Algorithms/Train.py
+++ Algorithms/Train.py
@@ -3,9 +3,9 @@ import copy
 def SimpleGradientDescent(activations, delta, weights, biases, eta):
     biases = np.subtract(biases, np.multiply(eta, delta))
     x = copy.deepcopy(activations[1:])
-    for i in range(1, len(activations)-1):
+    for i in range(0, len(activations)-1):
         x[i] = np.multiply(activations[i], delta[i])
-    for i in range(0, len(x)-1):
-        x[i] = np.reshape(x[i], np.shape(weights[i]))
+    for i in range(0, len(x)):
+        x[i] = np.reshape(x[i], np.shape(weights[i]))    
     weights = np.subtract(weights, x)
-    return weights, biases
+    return weights, biases, x
