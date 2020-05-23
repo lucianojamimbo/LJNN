812cd686b87266c28a5a39dd4c6143d60d4bfd35
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-23 20:10:16 +0100

    added a system for epochs

--- main.py
+++ main.py
@@ -8,15 +8,27 @@ weights, biases, delta = ljnnb.init(sizes)
 data = ld.ld()
 a, ps = ljnnb.feedforwards(weights, biases, data[0][0])
 graph = []
-i = 0
-while i < 60000:
-    do = [0,0,0,0,0,0,0,0,0,0]
-    do[data[i][1][0]] = 1
-    a, ps = ljnnb.feedforwards(weights, biases, data[i][0])
-    delta = ljnnb.geterror(a, do, ps, delta, weights, biases)
-    weights, biases, x = train.SimpleGradientDescent(a, delta, weights, biases, 1)
-    graph.append(ljnnb.cost(do, a[-1]))
-    i+=1    
+epochs = 0
+while epochs < 5:
+    i = 0
+    while i < 60000:
+        do = [0,0,0,0,0,0,0,0,0,0]
+        do[data[i][1][0]] = 1
+        a, ps = ljnnb.feedforwards(weights, biases, data[i][0])
+        delta = ljnnb.geterror(a, do, ps, delta, weights, biases)
+        weights, biases, x = train.SimpleGradientDescent(a, delta, weights, biases, 1)
+        graph.append(ljnnb.cost(do, a[-1]))
+        i+=1  
+    print("epoch complete")
+    epochs+=1
 import matplotlib.pyplot as plt
 plt.plot(graph)
 plt.show()
+correct = 0
+i = 0
+while i < 10000:
+    a, ps = ljnnb.feedforwards(weights, biases, data[i][0])
+    if data[i][1][0] == np.argmax(a[-1]):
+        correct+=1
+    i+=1    
+print("amount of images correctly classified out of 10000:", correct)
