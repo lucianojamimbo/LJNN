2f37e404ef9fbb21c97f31716c88487b82deece1
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-23 15:03:02 +0100

    removed the fucking stupid [[itm] for itm in image] bullshit

--- loadmnist.py
+++ loadmnist.py
@@ -1,7 +1,6 @@
 import numpy as np
 import gzip
 def ld():
-    print("loading and preparing MNIST train files")
     imgs = gzip.open('Datasets/train-images-idx3-ubyte.gz', 'r')
     labs = gzip.open('Datasets/train-labels-idx1-ubyte.gz', 'r')
     imgs.read(16) #skip some data (this data can be useful but its more work to read it than to just hard code a couple numbers)
@@ -12,18 +11,11 @@ def ld():
         buf = imgs.read(28 * 28 * 1)
         image = np.frombuffer(buf, dtype=np.uint8)
         image = np.divide(image, 255)
-        image = [[itm] for itm in image]
+        #image = [[itm] for itm in image] #this is stupid and bad and i should modify other functions instead of doing this
         labelbuf = labs.read(1)
         label = np.frombuffer(labelbuf, dtype=np.uint8)
         imglst.append((image, label))
         i+=1
     data = np.asarray(imglst)
     del imglst #die
-    print("MNIST train files loaded and prepared")
     return data
-
-import time
-start = time.time()
-x = ld()
-end = time.time()
-print("time taken:", end-start)
