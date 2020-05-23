d95dc560e9e0524a2353af9f16b391230d3e0dec
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-23 15:02:29 +0100

    Functioning simple gradient descent!

--- Algorithms/Train.py
+++ Algorithms/Train.py
@@ -1,6 +1,11 @@
 import numpy as np
-import copy
+import copy 
 def SimpleGradientDescent(activations, delta, weights, biases, eta):
-    weights = np.subtract(weights, np.multiply(eta, np.multiply(activations[1:], delta)))
     biases = np.subtract(biases, np.multiply(eta, delta))
+    x = copy.deepcopy(activations[1:])
+    for i in range(1, len(activations)-1):
+        x[i] = np.multiply(activations[i], delta[i])
+    for i in range(0, len(x)-1):
+        x[i] = np.reshape(x[i], np.shape(weights[i]))
+    weights = np.subtract(weights, x)
     return weights, biases
