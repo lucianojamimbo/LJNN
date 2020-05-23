18720a1f60d03829b8ce34effe820b2da64cb99b
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-23 21:07:13 +0100

    renamed x to nabla_w

--- Algorithms/Train.py
+++ Algorithms/Train.py
@@ -2,10 +2,10 @@ import numpy as np
 import copy 
 def SimpleGradientDescent(activations, delta, weights, biases, eta):
     biases = np.subtract(biases, np.multiply(eta, delta))
-    x = copy.deepcopy(activations[1:])
+    nabla_w = copy.deepcopy(activations[1:])
     for i in range(0, len(activations)-1):
-        x[i] = np.multiply(activations[i], delta[i])
-    for i in range(0, len(x)):
-        x[i] = np.reshape(x[i], np.shape(weights[i]))    
-    weights = np.subtract(weights, x)
-    return weights, biases, x
+        nabla_w[i] = np.multiply(activations[i], delta[i])
+    for i in range(0, len(nabla_w)):
+        nabla_w[i] = np.reshape(nabla_w[i], np.shape(weights[i]))    
+    weights = np.subtract(weights, nabla_w)
+    return weights, biases
