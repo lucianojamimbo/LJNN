1afbc7f545d32cb2aa6d9b5f489954ef6b32a494
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-23 15:02:37 +0100

    changed to test simple GD

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ import numpy as np
 import Functions.LJNNbasic as ljnnb
 import Algorithms.Train as train
 np.random.seed(0)
+
+
+#this all works
 sizes = [1,2,3]
-
 weights, biases, delta = ljnnb.init(sizes)
+a, ps = ljnnb.feedforwards(weights, biases, [0])
 
-a, ps = ljnnb.feedforwards(weights, biases, [[1]])
-
-do = [[1],[1],[1]]
-
-delta = ljnnb.geterror(a, do, ps, delta, weights)
+graph = []
 i = 0
 while i < 1000:
-    a, ps = ljnnb.feedforwards(weights, biases, [[1]])
-    delta = ljnnb.geterror(a, do, ps, delta, weights)    
-    weights, biases = train.SimpleGradientDescent(a, delta, weights, biases, 2)
+    do = [1,1,1]
+    delta = ljnnb.geterror(a, do, ps, delta, weights, biases)
+    weights, biases = train.SimpleGradientDescent(a, delta, weights, biases, 1)
+    a, ps = ljnnb.feedforwards(weights, biases, [0])
+    graph.append(a[-1])
     i+=1
+    
+import matplotlib.pyplot as plt
+
+plt.plot(graph)
+plt.show()
