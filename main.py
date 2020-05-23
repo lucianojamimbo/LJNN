fe112d48894644c13aab43352b8e472c0cf4a501
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-23 19:29:57 +0100

    changes to test simple GD

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 import numpy as np
 import Functions.LJNNbasic as ljnnb
 import Algorithms.Train as train
+import loadmnist as ld
 np.random.seed(0)
-
-
-#this all works
-sizes = [1,2,3]
+sizes = [784,16,10]
 weights, biases, delta = ljnnb.init(sizes)
-a, ps = ljnnb.feedforwards(weights, biases, [0])
-
+data = ld.ld()
+a, ps = ljnnb.feedforwards(weights, biases, data[0][0])
 graph = []
 i = 0
-while i < 1000:
-    do = [1,1,1]
+while i < 60000:
+    do = [0,0,0,0,0,0,0,0,0,0]
+    do[data[i][1][0]] = 1
+    a, ps = ljnnb.feedforwards(weights, biases, data[i][0])
     delta = ljnnb.geterror(a, do, ps, delta, weights, biases)
-    weights, biases = train.SimpleGradientDescent(a, delta, weights, biases, 1)
-    a, ps = ljnnb.feedforwards(weights, biases, [0])
-    graph.append(a[-1])
-    i+=1
-    
+    weights, biases, x = train.SimpleGradientDescent(a, delta, weights, biases, 1)
+    graph.append(ljnnb.cost(do, a[-1]))
+    i+=1    
 import matplotlib.pyplot as plt
-
 plt.plot(graph)
 plt.show()
