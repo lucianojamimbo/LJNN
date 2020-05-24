4cd352096dc472ec74d0774f5604fa910763d364
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-24 14:02:09 +0100

    changed some settings

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import Functions.LJNNbasic as ljnnb
 import Algorithms.Train as train
 import loadmnist as ld
 np.random.seed(234)
-sizes = [784,32,16,10] #set the shape of the network
+sizes = [784,32,10] #set the shape of the network
 weights, biases, delta = ljnnb.init(sizes) #initialise the weights, biases, and delta randomly but with the correct shape
 print("loading train data")
 data = ld.ldtrain()
@@ -12,5 +12,5 @@ print("loading test data")
 testdata = ld.ldtest()
 print("test data loaded")
 a, ps = ljnnb.feedforwards(weights, biases, data[0][0])
-weights, biases = train.StochasticGradientDescent(a, ps, delta, weights, biases, 2, 20, 30, data, testdata)
+weights, biases = train.StochasticGradientDescent(a, ps, delta, weights, biases, 3, 10, 30, data, testdata)
 print("amount of images correctly classified out of 10000:", ljnnb.test(testdata, weights, biases))
