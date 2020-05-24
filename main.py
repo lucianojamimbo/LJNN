31bf2e2f05b34fee6b3d30041e3780b2dea25a0f
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-24 12:54:01 +0100

    main now runs the new stochastic gradient descent

--- main.py
+++ main.py
@@ -2,39 +2,15 @@ import numpy as np
 import Functions.LJNNbasic as ljnnb
 import Algorithms.Train as train
 import loadmnist as ld
-np.random.seed(0)
-def test():
-    print("testing")
-    correct = 0
-    i = 0
-    while i < 10000:
-        a, ps = ljnnb.feedforwards(weights, biases, data[i][0])
-        if data[i][1][0] == np.argmax(a[-1]):
-            correct+=1
-        i+=1
-    print("test complete")
-    return correct    
-sizes = [784,32,10]
-weights, biases, delta = ljnnb.init(sizes)
+np.random.seed(234)
+sizes = [784,32,16,10] #set the shape of the network
+weights, biases, delta = ljnnb.init(sizes) #initialise the weights, biases, and delta randomly but with the correct shape
 print("loading train data")
-data = ld.ld()
+data = ld.ldtrain()
 print("train data loaded")
+print("loading test data")
+testdata = ld.ldtest()
+print("test data loaded")
 a, ps = ljnnb.feedforwards(weights, biases, data[0][0])
-graph = []
-epochs = 0
-while epochs < 20:
-    i = 0
-    while i < 60000:
-        do = [0,0,0,0,0,0,0,0,0,0]
-        do[data[i][1][0]] = 1
-        a, ps = ljnnb.feedforwards(weights, biases, data[i][0])
-        delta = ljnnb.geterror(a, do, ps, delta, weights, biases)
-        weights, biases = train.SimpleGradientDescent(a, delta, weights, biases, 0.5)
-        i+=1  
-    print("epoch {0} complete".format(epochs))
-    graph.append(test())
-    epochs+=1
-import matplotlib.pyplot as plt
-plt.plot(graph)
-plt.show()
-print("amount of images correctly classified out of 10000:", test())
+weights, biases = train.StochasticGradientDescent(a, ps, delta, weights, biases, 5, 30, 10, data, testdata)
+print("amount of images correctly classified out of 10000:", ljnnb.test(testdata, weights, biases))
