f94b596a154962e9435115cd40cca67e70c42789
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-24 12:53:36 +0100

    added a loader for test data

--- loadmnist.py
+++ loadmnist.py
@@ -1,6 +1,6 @@
 import numpy as np
 import gzip
-def ld():
+def ldtrain():
     imgs = gzip.open('Datasets/train-images-idx3-ubyte.gz', 'r')
     labs = gzip.open('Datasets/train-labels-idx1-ubyte.gz', 'r')
     imgs.read(16) #skip some data (this data can be useful but its more work to read it than to just hard code a couple numbers)
@@ -11,10 +11,27 @@ def ld():
         buf = imgs.read(28 * 28 * 1)
         image = np.frombuffer(buf, dtype=np.uint8)
         image = np.divide(image, 255)
-        #image = [[itm] for itm in image] #this is stupid and bad and i should modify other functions instead of doing this
         labelbuf = labs.read(1)
         label = np.frombuffer(labelbuf, dtype=np.uint8)
         imglst.append((image, label))
+        i+=1
+    data = np.asarray(imglst)
+    del imglst #die
+    return data
+def ldtest():
+    testimgs = gzip.open('Datasets/t10k-images-idx3-ubyte.gz', 'r')
+    testlabs = gzip.open('Datasets/t10k-labels-idx1-ubyte.gz', 'r')
+    testimgs.read(16) #skip some data (this data can be useful but its more work to read it than to just hard code a couple numbers)
+    testlabs.read(8) #skip some data (this data can be useful but its more work to read it than to just hard code a couple numbers)
+    imglst = []
+    i = 0
+    while i < 10000:
+        testbuf = testimgs.read(28 * 28 * 1)
+        testimage = np.frombuffer(testbuf, dtype=np.uint8)
+        testimage = np.divide(testimage, 255)
+        testlabelbuf = testlabs.read(1)
+        testlabel = np.frombuffer(testlabelbuf, dtype=np.uint8)
+        imglst.append((testimage, testlabel))
         i+=1
     data = np.asarray(imglst)
     del imglst #die
