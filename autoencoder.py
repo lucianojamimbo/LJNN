0cd18f1e3322811057cfe19bc3a3735970b11478
lucianojamimbo <lucianojamimbo@gmail.com>
2020-05-25 21:14:54 +0200

    added a function to compress a dataset (might need modification for anything that isnt mnist)

--- autoencoder.py
+++ autoencoder.py
@@ -1,6 +1,8 @@
 import Functions.LJNNbasic as ljnnb
 import loadmnist as ld
 import pickle
+import matplotlib.pyplot as plt
+import numpy as np
 data = ld.ldtrain()
 testdata = ld.ldtest()
 encoderweights = pickle.load(open("data/encoderweights.pickle", "rb"))
@@ -13,3 +15,13 @@ def encode(inputdata):
 def decode(inputdata):
     a, ps = ljnnb.feedforwards(decoderweights, decoderbiases, inputdata)
     return a[-1]
+def compressdataset(data):
+    i = 0
+    while i < len(data):
+        print(i)
+        data[i][0] = encode(data[i][0])
+        i+=1
+    return data
+data = compressdataset(data)
+with open("Data/compressedMNIST.pickle", "wb") as f:
+    pickle.dump(data, f)
